7b46ac767357d68bb7ac92a5b880a4e6e5974fdf
zetoirque <89418946+zetoirque@users.noreply.github.com>
2022-10-02 19:52:04 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -7,8 +7,8 @@ function Cheat()
     local Main = Window:NewTab("Main")
     local MainSection = Main:NewSection("Auto Farme")
 
-    local Player = Window:NewTab("Player")
-    local PlayerSection = Player:NewSection("Player")
+    local Player = Window:NewTab("ZIZIIZIZIZI")
+    local PlayerSection = Player:NewSection("ZIZIZIIZIZI")
 
     local Info = Window:NewTab("Info")
     local InfoSection = Info:NewSection("Info")
