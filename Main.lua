cf356887096e6fb3fb28799bd5ee8614a9406c54
zetoirque <89418946+zetoirque@users.noreply.github.com>
2022-10-02 19:47:30 +0200

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,366 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+
+
+function Cheat()
+    local Window = Library.CreateLib("SCRIPT BY ZETOIRQUE", "GrapeTheme")
+
+    local Main = Window:NewTab("Main")
+    local MainSection = Main:NewSection("Auto Farme")
+
+    local Player = Window:NewTab("Player")
+    local PlayerSection = Player:NewSection("Player")
+
+    local Info = Window:NewTab("Info")
+    local InfoSection = Info:NewSection("Info")
+
+    --Togle
+     InfoSection:NewKeybind("Togle Gui", "Press Right Ctrl For Toggle Gui", Enum.KeyCode.RightControl, function()
+        Library:ToggleUI()
+    end)
+
+    --Button Player
+
+    --Button Auto Farme
+
+
+    --World #1
+    MainSection:NewButton("Auto Shuriken World #1", "", function()
+
+        --Tp Good Location
+
+        local teleport_table1 = {
+
+            location1 = Vector3.new(-1.65027, 3, -0.769696)
+        
+        }
+        
+        local tween_s = game:GetService('TweenService')
+        local tween_i = TweenInfo.new(1,Enum.EasingStyle.Linear)
+        
+        local lp = game.Players.LocalPlayer
+        
+        function bypass_teleporter1(v)
+        
+            if lp.Character and lp.Character:FindFirstChild('HumanoidRootPart') then
+        
+                local cf = CFrame.new(v)
+                local a = tween_s:Create(lp.Character.HumanoidRootPart, tween_i, {CFrame=cf})
+        
+                a:Play()
+        
+        
+        
+            end
+        
+        end
+        
+        bypass_teleporter1(teleport_table1.location1)
+
+        wait(2)
+
+        --Finish
+        local teleport_table = {
+
+            location1 = Vector3.new(-1.65027, 3, 780087)
+        
+        }
+        
+        local tween_s = game:GetService('TweenService')
+        local tween_i = TweenInfo.new(15,Enum.EasingStyle.Linear)
+        
+        local lp = game.Players.LocalPlayer
+        
+        function bypass_teleporter(v)
+        
+            if lp.Character and lp.Character:FindFirstChild('HumanoidRootPart') then
+        
+                local cf = CFrame.new(v)
+                local a = tween_s:Create(lp.Character.HumanoidRootPart, tween_i, {CFrame=cf})
+        
+                a:Play()
+        
+        
+        
+            end
+        
+        end
+        
+        bypass_teleporter(teleport_table.location1)          
+
+    end)
+
+
+    --World #2
+    MainSection:NewButton("Auto Shuriken World #2", "", function()
+
+
+        --Tp Good Location
+
+        local teleport_table1 = {
+
+            location1 = Vector3.new(248.03, 3, -0.769696)
+        
+        }
+        
+        local tween_s = game:GetService('TweenService')
+        local tween_i = TweenInfo.new(1,Enum.EasingStyle.Linear)
+        
+        local lp = game.Players.LocalPlayer
+        
+        function bypass_teleporter1(v)
+        
+            if lp.Character and lp.Character:FindFirstChild('HumanoidRootPart') then
+        
+                local cf = CFrame.new(v)
+                local a = tween_s:Create(lp.Character.HumanoidRootPart, tween_i, {CFrame=cf})
+        
+                a:Play()
+        
+        
+        
+            end
+        
+        end
+        
+        bypass_teleporter1(teleport_table1.location1)
+
+        wait(2)
+
+        --Finish
+        local teleport_table = {
+
+            location1 = Vector3.new(248.03, 3, 780087)
+        
+        }
+        
+        local tween_s = game:GetService('TweenService')
+        local tween_i = TweenInfo.new(15,Enum.EasingStyle.Linear)
+        
+        local lp = game.Players.LocalPlayer
+        
+        function bypass_teleporter(v)
+        
+            if lp.Character and lp.Character:FindFirstChild('HumanoidRootPart') then
+        
+                local cf = CFrame.new(v)
+                local a = tween_s:Create(lp.Character.HumanoidRootPart, tween_i, {CFrame=cf})
+        
+                a:Play()
+        
+        
+        
+            end
+        
+        end
+        
+        bypass_teleporter(teleport_table.location1)          
+
+    end)
+
+
+    --World #3
+    MainSection:NewButton("Auto Shuriken World #3", "", function()
+
+
+        --Tp Good Location
+
+        local teleport_table1 = {
+
+            location1 = Vector3.new(499.324, 3, -0.769696)
+        
+        }
+        
+        local tween_s = game:GetService('TweenService')
+        local tween_i = TweenInfo.new(1,Enum.EasingStyle.Linear)
+        
+        local lp = game.Players.LocalPlayer
+        
+        function bypass_teleporter1(v)
+        
+            if lp.Character and lp.Character:FindFirstChild('HumanoidRootPart') then
+        
+                local cf = CFrame.new(v)
+                local a = tween_s:Create(lp.Character.HumanoidRootPart, tween_i, {CFrame=cf})
+        
+                a:Play()
+        
+        
+        
+            end
+        
+        end
+        
+        bypass_teleporter1(teleport_table1.location1)
+
+        wait(2)
+
+        --Finish
+        local teleport_table = {
+
+            location1 = Vector3.new(499.324, 3, 780087)
+        
+        }
+        
+        local tween_s = game:GetService('TweenService')
+        local tween_i = TweenInfo.new(15,Enum.EasingStyle.Linear)
+        
+        local lp = game.Players.LocalPlayer
+        
+        function bypass_teleporter(v)
+        
+            if lp.Character and lp.Character:FindFirstChild('HumanoidRootPart') then
+        
+                local cf = CFrame.new(v)
+                local a = tween_s:Create(lp.Character.HumanoidRootPart, tween_i, {CFrame=cf})
+        
+                a:Play()
+        
+        
+        
+            end
+        
+        end
+            
+        bypass_teleporter(teleport_table.location1)          
+
+    end)
+    
+
+    --World #4
+    MainSection:NewButton("Auto Shuriken World #4", "", function()
+
+
+         --Tp Good Location
+
+         local teleport_table1 = {
+
+            location1 = Vector3.new(749.754, 3, -0.769696)
+        
+        }
+        
+        local tween_s = game:GetService('TweenService')
+        local tween_i = TweenInfo.new(1,Enum.EasingStyle.Linear)
+        
+        local lp = game.Players.LocalPlayer
+        
+        function bypass_teleporter1(v)
+        
+            if lp.Character and lp.Character:FindFirstChild('HumanoidRootPart') then
+        
+                local cf = CFrame.new(v)
+                local a = tween_s:Create(lp.Character.HumanoidRootPart, tween_i, {CFrame=cf})
+        
+                a:Play()
+        
+        
+        
+            end
+        
+        end
+        
+        bypass_teleporter1(teleport_table1.location1)
+
+        wait(2)
+
+        --Finish
+        local teleport_table = {
+
+            location1 = Vector3.new(749.754, 3, 780087)
+        
+        }
+        
+        local tween_s = game:GetService('TweenService')
+        local tween_i = TweenInfo.new(15,Enum.EasingStyle.Linear)
+        
+        local lp = game.Players.LocalPlayer
+        
+        function bypass_teleporter(v)
+        
+            if lp.Character and lp.Character:FindFirstChild('HumanoidRootPart') then
+        
+                local cf = CFrame.new(v)
+                local a = tween_s:Create(lp.Character.HumanoidRootPart, tween_i, {CFrame=cf})
+        
+                a:Play()
+        
+        
+        
+            end
+        
+        end
+            
+        bypass_teleporter(teleport_table.location1)          
+
+    end)
+
+
+    --World #5
+    MainSection:NewButton("Auto Shuriken World #5", "", function()
+        
+
+        --Tp Good Location
+
+        local teleport_table1 = {
+
+            location1 = Vector3.new(997.532, 3, -0.769696)
+        
+        }
+        
+        local tween_s = game:GetService('TweenService')
+        local tween_i = TweenInfo.new(1,Enum.EasingStyle.Linear)
+        
+        local lp = game.Players.LocalPlayer
+        
+        function bypass_teleporter1(v)
+        
+            if lp.Character and lp.Character:FindFirstChild('HumanoidRootPart') then
+        
+                local cf = CFrame.new(v)
+                local a = tween_s:Create(lp.Character.HumanoidRootPart, tween_i, {CFrame=cf})
+        
+                a:Play()
+        
+        
+        
+            end
+        
+        end
+        
+        bypass_teleporter1(teleport_table1.location1)
+
+        wait(2)
+
+        --Finish
+        local teleport_table = {
+
+            location1 = Vector3.new(997.532, 3, 780087)
+        
+        }
+        
+        local tween_s = game:GetService('TweenService')
+        local tween_i = TweenInfo.new(15,Enum.EasingStyle.Linear)
+        
+        local lp = game.Players.LocalPlayer
+        
+        function bypass_teleporter(v)
+        
+            if lp.Character and lp.Character:FindFirstChild('HumanoidRootPart') then
+        
+                local cf = CFrame.new(v)
+                local a = tween_s:Create(lp.Character.HumanoidRootPart, tween_i, {CFrame=cf})
+        
+                a:Play()
+        
+        
+        
+            end
+        
+        end            
+
+            bypass_teleporter(teleport_table.location1)
+
+    end)
+
+end
+
+Cheat()
