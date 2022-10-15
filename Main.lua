207881a59bfd68c700ae5d09975f3221160c33c5
zetoirque <89418946+zetoirque@users.noreply.github.com>
2022-10-15 11:18:06 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -490,6 +490,74 @@ function Cheat()
 
     end)
 
+
+        --World #7
+        MainSection:NewButton("Auto Shuriken World #7", "", function()
+        
+
+            --Tp Good Location
+    
+            local teleport_table1 = {
+    
+                location1 = Vector3.new(1499, 3, -0.769696)
+            
+            }
+            
+            local tween_s = game:GetService('TweenService')
+            local tween_i = TweenInfo.new(1,Enum.EasingStyle.Linear)
+            
+            local lp = game.Players.LocalPlayer
+            
+            function bypass_teleporter1(v)
+            
+                if lp.Character and lp.Character:FindFirstChild('HumanoidRootPart') then
+            
+                    local cf = CFrame.new(v)
+                    local a = tween_s:Create(lp.Character.HumanoidRootPart, tween_i, {CFrame=cf})
+            
+                    a:Play()
+            
+            
+            
+                end
+            
+            end
+            
+            bypass_teleporter1(teleport_table1.location1)
+    
+            wait(2)
+    
+            --Finish
+            local teleport_table = {
+    
+                location1 = Vector3.new(1499, 3, 780087)
+            
+            }
+            
+            local tween_s = game:GetService('TweenService')
+            local tween_i = TweenInfo.new(15,Enum.EasingStyle.Linear)
+            
+            local lp = game.Players.LocalPlayer
+            
+            function bypass_teleporter(v)
+            
+                if lp.Character and lp.Character:FindFirstChild('HumanoidRootPart') then
+            
+                    local cf = CFrame.new(v)
+                    local a = tween_s:Create(lp.Character.HumanoidRootPart, tween_i, {CFrame=cf})
+            
+                    a:Play()
+            
+            
+            
+                end
+            
+            end            
+    
+                bypass_teleporter(teleport_table.location1)
+    
+        end)
+
 end
 
 Cheat()
